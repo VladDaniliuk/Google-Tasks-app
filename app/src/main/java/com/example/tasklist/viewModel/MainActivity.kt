18306724e985bcd603c3959b13c533a0b9e9c0ba
Student <student@ws-122-11b.local>
2021-04-09 18:48:14 +0300

    Google Sign In fragments

--- app/src/main/java/com/example/tasklist/viewModel/MainActivity.kt
+++ app/src/main/java/com/example/tasklist/viewModel/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.tasklist.viewModel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.tasklist.R
+import com.example.tasklist.Task
+import com.example.tasklist.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<SignInFragment>(R.id.fragment_container_view)
+            /*val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 1)*/
+        }
+
+        /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information
+        }
+    }*/
+
+
     }
 }
---
+++ app/src/main/java/com/example/tasklist/viewModel/SignInFragment.kt
@@ -0,0 +1,7 @@
+package com.example.tasklist.viewModel
+
+import androidx.fragment.app.Fragment
+import com.example.tasklist.R
+
+class SignInFragment: Fragment(R.layout.fragment_sign_in) {
+}
